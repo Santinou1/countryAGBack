39db9714fb297a0bc036d4cbeb21f203c8bde13e
Santinou1 <santitech445@gmail.com>
2025-06-17 11:20:51 -0300

    portRnv

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  config()
+
   // Configurar el prefijo global 'api'
   app.setGlobalPrefix('api');
   
