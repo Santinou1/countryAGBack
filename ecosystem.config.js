1de896900c98ec96b484764c6a031ea2e5918d2b
Santinou1 <santitech445@gmail.com>
2025-06-18 16:33:18 -0300

    te

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,10 @@
+// ecosystem.config.js
 module.exports = {
     apps: [
       {
         name: 'countryAG-back',
-        script: 'src/main.ts',
-        interpreter: 'node',
-        exec_mode: 'fork',
-        instances: 1,
-        watch: false,
-        interpreter_args: '-r ts-node/register',
+        script: 'npm',
+        args: 'start',
         env: {
           B_HOST: '172.31.50.156',
           DB_PORT: '3306',
